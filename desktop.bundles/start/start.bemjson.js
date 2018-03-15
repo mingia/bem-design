b01430f1a95ca0f2b1d64e03b75fde61883958ba
Михаил О. Колосков <m.koloskov@live.ru>
2018-03-15 11:30:02 +0300

    Add start

--- desktop.bundles/start/start.bemjson.js
+++ desktop.bundles/start/start.bemjson.js
@@ -1,37 +1,22 @@
 module.exports = {
 	block: 'page',
-	title: 'BEM New',
+	title: 'BEM Start',
 	favicon: '/favicon.ico',
 	lang: 'ru',
 	head: [
 		{ elem: 'meta', attrs: { name: 'description', content: '' } },
 		{ elem: 'meta', attrs: { name: 'viewport', content: 'width=device-width, initial-scale=1' } },
-		{ elem: 'css', url: 'new.min.css' },
+		{ elem: 'css', url: 'start.min.css' },
 		{ elem: 'css', url: '/assets/ibm-type/css/ibm-type.css' },
 		{ elem: 'js', url: 'https://code.jquery.com/jquery-3.2.1.js' },
 	],
-	scripts: [{ elem: 'js', url: 'new.min.js' }],
+	scripts: [{ elem: 'js', url: 'start.min.js' }],
 	mods: { theme: 'islands' },
 	mix: {
 		block: 'theme',
-		mods: { color: 'default', space: 'default', size: 'default', gap: 'small', menu: 'default' }
+		mods: { color: 'default', space: 'default', size: 'default' }
 	},
 	content: [
-		{
-			block: 'hello',
-			content: [
-				{
-					elem: 'name',
-					elemMods: { view: 'inverse' },
-					content: [
-						{
-							block: 'text',
-							mods: { view: 'inverse' },
-							content: 'Inverse'
-						}
-					]
-				}
-			]
-		}
+
 	]
 };
