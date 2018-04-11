61c8ee791a83f09611ae7b507d267044a40f4b56
Михаил О. Колосков <m.koloskov@live.ru>
2018-04-11 12:06:18 +0300

    Update

---
+++ desktop.bundles/components/components.bemjson.js
@@ -0,0 +1,359 @@
+module.exports = {
+	block: 'page',
+	title: 'BEM components',
+	favicon: '/favicon.ico',
+	lang: 'ru',
+	head: [
+		{ elem: 'meta', attrs: { name: 'description', content: '' } },
+		{ elem: 'meta', attrs: { name: 'viewport', content: 'width=device-width, initial-scale=1' } },
+		{ elem: 'css', url: 'components.min.css' },
+		{ elem: 'js', url: 'https://code.jquery.com/jquery-3.2.1.js' },
+	],
+	scripts: [{ elem: 'js', url: 'components.min.js' }],
+	mods: { type: 'full-screen', theme: 'islands' },
+	mix: {
+		block: 'theme',
+		mods: { color: 'bem', space: 'default', size: 'default', gap: 'small', menu: 'default', font: 'ibm' }
+	},
+	content: [
+		{
+			block: 'demo',
+			mix: [
+				{ block: 'theme', mods: { color: 'default', space: 'default', size: 'default' }},
+			],
+			content: [
+				{
+					block: 'tpl-layout',
+					content: [
+						{
+							elem: 'container',
+							elemMods: { size: 'xs', align: 'center' },
+							content: [
+								{
+									block: 'controls',
+									content: [
+										{
+											elem: 'section',
+											content: [
+												{
+													elem: 'item',
+													elemMods: { width: 's' },
+													content: [
+														{
+															block: 'button',
+															mods: {
+																theme: 'islands',
+																size: 'm',
+																view: 'action',
+																width: 'available'
+															},
+															text: 'Купить сейчас!'
+														}
+													]
+												},
+												{
+													elem: 'item',
+													elemMods: { width: 's' },
+													content: [
+														{
+															block: 'button',
+															mods: {
+																theme: 'islands',
+																size: 'm',
+																width: 'available'
+															},
+															text: 'Размер m'
+														}
+													]
+												},
+												{
+													elem: 'item',
+													elemMods: { width: 's' },
+													content: [
+														{
+															block: 'button',
+															text: 'Return button',
+															mods: {
+																theme: 'islands',
+																size: 'm',
+																width: 'available'
+															},
+															icon: {
+																block: 'icon',
+																mods: {
+																	'arrow-left': 's-primary'
+																}
+															}
+														}
+													]
+												},
+												{
+													elem: 'item',
+													elemMods: { width: 's' },
+													content: [
+														{
+															block: 'button',
+															mods: {
+																theme: 'islands',
+																size: 'm',
+																view: 'pseudo',
+																width: 'available'
+															},
+															text: 'У меня прозрачный фон'
+														}
+													]
+												},
+											]
+										},
+										{
+											elem: 'section',
+											content: [
+												{
+													elem: 'item',
+													content: [
+														{
+													    block: 'select',
+													    mods: {
+													        mode: 'radio',
+													        theme: 'islands',
+													        width: 'available',
+													        size: 'm'
+													    },
+													    name: 'select1',
+													    val: 2,
+													    options: [
+													        {
+													            val: 1,
+													            text: 'Доклад'
+													        },
+													        {
+													            val: 2,
+													            text: 'Мастер-класс'
+													        },
+													        {
+													            val: 3,
+													            text: 'Круглый стол'
+													        }
+													    ]
+														}
+													]
+												},
+												{
+													elem: 'item',
+													content: [
+														{
+															block: 'select',
+															mods: {
+																	mode: 'radio-check',
+																	theme: 'islands',
+																	size: 'm',
+																	width: 'available',
+																	disabled: true
+															},
+															name: 'select4',
+															val: 2,
+															text: '—',
+															options: [
+																	{
+																			val: 1,
+																			text: 'Доклад'
+																	},
+																	{
+																			val: 2,
+																			text: 'Мастер-класс наоборот: вы пишете БЭМ-проект, а мы подсказываем'
+																	},
+																	{
+																			val: 3,
+																			text: 'Круглый стол'
+																	}
+															]
+														}
+													]
+												}
+											]
+										},
+										{
+											elem: 'section',
+											content: [
+												{
+													elem: 'item',
+													content: [
+														{
+															block: 'input',
+															mods: {
+																theme: 'islands',
+																size: 'm',
+																width: 'available',
+																'has-clear': true,
+															},
+															val: 'Привет!'
+														}
+													]
+												},
+												{
+													elem: 'item',
+													content: [
+														{
+															block: 'input',
+															mods: {
+																theme: 'islands',
+																size: 'm',
+																width: 'available',
+																disabled: true
+															},
+															placeholder: 'Введите имя'
+														}
+													]
+												}
+											]
+										},
+										{
+											elem: 'section',
+											content: [
+												{
+													elem: 'item',
+													content: [
+														{
+															block: 'textarea',
+															mods: {
+																theme: 'islands',
+																size: 'm',
+																width: 'available'
+															},
+															placeholder: 'Ваш текст должен быть здесь'
+														}
+													]
+												},
+												{
+													elem: 'item',
+													content: [
+														{
+															block: 'textarea',
+															mods: {
+																theme: 'islands',
+																size: 'm',
+																disabled: true,
+																width: 'available'
+															},
+															placeholder: 'Ваш текст должен быть здесь'
+														}
+													]
+												}
+											]
+										},
+
+										{
+											elem: 'section',
+											content: [
+												{
+													elem: 'item',
+													content: [
+														{
+															block: 'checkbox',
+															mods: {
+																theme: 'islands',
+																size: 'm',
+																checked: true
+															},
+															name: 'name1',
+															val: 'val_1',
+															text: 'Конфеты'
+														}
+													]
+												},
+												{
+													elem: 'item',
+													content: [
+														{
+															block: 'checkbox',
+															mods: {
+																theme: 'islands',
+																size: 'm',
+															},
+															name: 'name1',
+															val: 'val_1',
+															text: 'Конфеты'
+														}
+													]
+												},
+												{
+													elem: 'item',
+													content: [
+														{
+															block: 'checkbox',
+															mods: {
+																theme: 'islands',
+																size: 'm',
+																disabled: true
+															},
+															name: 'name1',
+															val: 'val_0',
+															text: 'Конфеты закончились'
+														}
+													]
+												}
+											]
+										},
+										{
+											elem: 'section',
+											content: [
+												{
+													elem: 'item',
+													content: [
+														{
+															block: 'radio',
+															mods: {
+																theme: 'islands',
+																size: 'm',
+																checked: true
+															},
+															name: 'radio-islands',
+															val: 'BEMHTML',
+															text: 'Использовать BEMHTML'
+														}
+													]
+												},
+												{
+													elem: 'item',
+													content: [
+														{
+															block: 'radio',
+															mods: {
+																theme: 'islands',
+																size: 'm',
+															},
+															name: 'radio-islands',
+															val: 'BEMHTML',
+															text: 'Использовать BEMHTML'
+														}
+													]
+												},
+												{
+													elem: 'item',
+													content: [
+														{
+															block: 'radio',
+															mods: {
+																theme: 'islands',
+																size: 'm',
+																disabled: true
+															},
+															name: 'radio-islands',
+															val: 'BEMHTML',
+															text: 'Использовать BEMHTML'
+														}
+													]
+												}
+											]
+										},
+								
+									]
+								}
+							]
+						}
+					]
+				}
+			]
+		}
+	]
+};
