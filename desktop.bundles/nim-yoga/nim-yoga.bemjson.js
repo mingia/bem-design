ca72ce95bc73d9f3952b9a3a6aba13b0e66e8c44
Александра А. Минджия <mindzhiya@yamoney.ru>
2018-04-16 22:36:40 +0300

    fg

--- desktop.bundles/nim-yoga/nim-yoga.bemjson.js
+++ desktop.bundles/nim-yoga/nim-yoga.bemjson.js
@@ -6,11 +6,11 @@ module.exports = {
 	head: [
 		{ elem: 'meta', attrs: { name: 'description', content: '' } },
 		{ elem: 'meta', attrs: { name: 'viewport', content: 'width=device-width, initial-scale=1' } },
-		{ elem: 'css', url: 'portfolio.min.css' },
+		{ elem: 'css', url: 'nim-yoga.min.css' },
 		{ elem: 'css', url: '/assets/ibm-type/css/ibm-type.css' },
 		{ elem: 'js', url: 'https://code.jquery.com/jquery-3.2.1.js' },
 	],
-	scripts: [{ elem: 'js', url: 'portfolio.min.js' }],
+	scripts: [{ elem: 'js', url: 'nim-yoga.min.js' }],
 	mods: { theme: 'islands' },
 	mix: {
 		block: 'theme',
